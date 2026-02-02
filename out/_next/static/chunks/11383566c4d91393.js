34acec22e53261cd8f5d3fe91529721cb21b25ec
saabir78611 <saabir78611@gmail.com>
2026-02-02 15:06:57 +0000

    docs: Add out directory to git history

---
+++ out/_next/static/chunks/11383566c4d91393.js
@@ -0,0 +1,9 @@
+__turbopack_load_page_chunks__("/shops/[id]", [
+  "static/chunks/d3855fb04f7ab4d8.js",
+  "static/chunks/fd07ec4dd650bd74.js",
+  "static/chunks/9585c37e137e6335.js",
+  "static/chunks/c72396bab077d2e5.js",
+  "static/chunks/7a79b4b2be11a634.js",
+  "static/chunks/7a54a949a17878d2.css",
+  "static/chunks/turbopack-81a9292bab15df9c.js"
+])
