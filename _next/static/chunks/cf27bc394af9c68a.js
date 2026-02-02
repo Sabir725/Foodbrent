a245e0463d55d26bf2ed8c9cdc0c177181a35664
saabir78611 <saabir78611@gmail.com>
2026-02-02 16:56:51 +0000

    fix: Disable checkout when time is not selected and fix build

--- _next/static/chunks/cf27bc394af9c68a.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/2134a30e52041ed8.js",
-  "static/chunks/7a79b4b2be11a634.js",
-  "static/chunks/c72396bab077d2e5.js",
-  "static/chunks/9585c37e137e6335.js",
-  "static/chunks/02504b6128670497.css",
-  "static/chunks/turbopack-c01492488af17cf3.js"
-])
